96500efe8bf99b128ec9e55d79d426edf1fc99fc
Arjun Masciarelli <arjun@masciarellis.com>
2025-09-04 11:52:52 -0400

    Added basic langchain functionality and first tool call through Polygon's API

--- stock_agent/apps/run_agent.py
+++ stock_agent/apps/run_agent.py
@@ -3,31 +3,20 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
-
-if not os.environ.get("OPENAI_API_KEY"):
-  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
-
-from langchain.chat_models import init_chat_model
-
-model = init_chat_model("gpt-4o-mini", model_provider="openai")
-
-
 # run main:
 if __name__ == "__main__":
     # you would keep taking the users input from cmd line and keep runing your code
-    agent = PolygonTools(llm_company_name="OpenAI", model_llm_name="GPT 5")
+    agent = PolygonTools(llm_company_name="OpenAI", model_llm_name="gpt-5-nano-2025-08-07")    
+
     # 1.) create infinite loop till the user exits
     # 2.) for each iteration take the users question from the cmd line
     # 3.) take that question and run it through your LLM -> call a tool if needed -> tool function will call the API -> return to the llm and the LLM would take the question of the user and the data from the API and answer the question
     
-    # Step one initialize Langchain and get the user able to prompt the bot through CLI
-
-    userquery = "get price of APPL price"
+    userquery = "Get the recent price of AAPL"
     print(agent.run(userquery))
 
-    userquery = "today open"
+    userquery = "What is NVDAs last day look like?"
     print(agent.run(userquery))
 
-    userquery = "price of blah"
+    userquery = "Get a stock description of MongoDB"
     print(agent.run(userquery))
--- stock_agent/pkg/types/base_agent.py
+++ stock_agent/pkg/types/base_agent.py
@@ -1,32 +1,57 @@
 from abc import ABC, abstractmethod
+import os
+from dotenv import load_dotenv
+
+from langchain.chat_models import init_chat_model
+from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
+from langchain_core.tools import tool
 
 
 class BaseStockAgent(ABC):
     def __init__(self, llm_company_name: str, model_llm_name:str):
-        self.llm_company_name = llm_company_name
-        self.model_llm_name = model_llm_name
-        # create llm using langchain
-        # TODO: use lanchaing to create the self.langchain (model, company whatever)
+        load_dotenv()
+        os.environ.get("OPENAI_API_KEY")
+        
+        # Define tools
+        @tool        
+        def get_ticker_price(ticker_name: str) -> str:
+            """Return OHLC stock data for a ticker as a JSON string."""
+            return self.get_ticker_price(ticker_name)
 
+        @tool
+        def get_ticker_desc(ticker_name: str) -> str:
+            """Return an overview of information about a company as a JSON string."""
+            return self.get_ticker_desc(ticker_name)             
 
-    def run(self, user_query: str) -> str:
-        # 1.) call the llm with user query:
-        # 2.) see if there is an an llm tool call
-        # 3.) if yes (get_current_price llm will also provide you the input -> APPL, get_today_close_open_price)
-        price = user_query
-        if "get price" in user_query:
-            price = self.get_current_price(user_query)
-        if "today open" in user_query:
-            price = self.get_today_close_open_price(user_query)
-        return price
+        tools = [get_ticker_price,get_ticker_desc]
 
+        self.llm = init_chat_model(model_llm_name, model_provider= llm_company_name)
+        self.llm_with_tools = self.llm.bind_tools(tools)
+    
+    def run(self, user_query: str) -> str:        
+        # System prompt   
+        messages = [
+            SystemMessage(content="You are a helpful stock assistant. Use tools when needed."),
+            HumanMessage(content=user_query),
+        ] 
 
+        ai_msg = self.llm_with_tools.invoke(messages)
+
+        messages.append(ai_msg)
+        for tool_call in ai_msg.tool_calls:
+            selected_tool = {"get_ticker_price": self.get_ticker_price, "get_ticker_desc": self.get_ticker_desc}[tool_call["name"].lower()]
+            args = tool_call["args"]
+            tool_output = selected_tool(**args)
+            tool_output = str(tool_output)            
+            messages.append(ToolMessage(tool_output, tool_call_id=tool_call["id"]))
+        messages 
+    
+        return self.llm_with_tools.invoke(messages).content.strip()           
+        
     @abstractmethod
-    def get_current_price(self, ticker_name: str) -> float:
+    def get_ticker_price(self, ticker_name: str) -> str:
         pass
 
     @abstractmethod
-    def get_today_close_open_price(self, ticker_name: str) -> float:
+    def get_ticker_desc(self, ticker_name: str) -> str:
         pass
-
-
